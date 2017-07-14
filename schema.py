e70ca598c37e5dc25ab2c2613a99ff8970c51088
Mark Parolisi <mark@markparolisi.com>
2017-07-14 11:30:22 -0400

    updated schema for department query

--- schema.py
+++ schema.py
@@ -3,21 +3,24 @@ from graphene import relay
 from graphene_sqlalchemy import SQLAlchemyObjectType, SQLAlchemyConnectionField
 from models import Department as DepartmentModel, Employee as EmployeeModel
 
-
 class Department(SQLAlchemyObjectType):
     class Meta(object):
         model = DepartmentModel
-        interfaces = (relay.Node, )
+        interfaces = (relay.Node,)
+
+    name = graphene.String(description='The name of the department')
 
 
 class Employee(SQLAlchemyObjectType):
     class Meta(object):
         model = EmployeeModel
-        interfaces = (relay.Node, )
+        interfaces = (relay.Node,)
 
 
 class Query(graphene.ObjectType):
     node = relay.Node.Field()
     all_employees = SQLAlchemyConnectionField(Employee)
+    department = relay.Node.Field(Department)
+
 
 schema = graphene.Schema(query=Query)
